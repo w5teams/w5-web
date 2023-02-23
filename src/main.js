19f89d4e514f498e0f4f97f4a7d46c0421b3999b
sanjinhub <sanjinhub@gmail.com>
2023-02-23 17:12:49 +0800

    feat: test ci

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ Vue.use(VueCookies);
 Vue.config.productionTip = false;
 
 if (document.location.port == 8080) {
-  Vue.prototype.BaseURL = document.location.origin.replace("8080", "8888");
+  Vue.prototype.BaseURL = "http://dev-test.w5soar.com";
 } else {
   Vue.prototype.BaseURL = document.location.origin;
 }
